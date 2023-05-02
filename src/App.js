73bb3e3433b3c4422e6ab202db758343bfd07a92
guilhermedevmatos15 <devguilhermeferreira@gmail.com>
2023-05-01 21:01:54 -0300

    build + style: alternando o app.js e aplicando fonte certa

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 // ? Components
 
-// ? CSS
-import './styles/css/App.css';
-
 
 function App() {
    return (
-      <div className="App">
-
-      </div>
+      <>
+         
+      </>
    );
 }
 
