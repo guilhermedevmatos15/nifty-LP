ae62a250bec7aad58ea0912505b40dc176b7099c
guilhermedevmatos15 <devguilhermeferreira@gmail.com>
2023-05-01 21:00:02 -0300

    build: estrutura de pastas/arquivos feita

---
+++ src/components/defaultComponent.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// * Components
+
+// * CSS
+
+// * Images
+
+// * Icons
+
+
+const defaultComponent = props => {
+   return (
+      <div className='DefaultComponent'>
+
+      </div>
+   );
+}
+
+export default defaultComponent;
